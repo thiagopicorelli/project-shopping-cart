ed3230cbacb5dcc5d9518343ccd4a97c33ccd0a7
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-10-11 16:30:04 -0300

    Cria função fetchProducts e converte testes para funcionarem em async

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,7 +1,10 @@
-const fetchProducts = (query) => {
+const fetchProducts = async (query) => {
   if (query === undefined) {
     throw new Error('You must provide an url');
   }
+
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+         .then((response) => response.json());
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -7,17 +7,20 @@ describe('1 - Teste a função fetchProducts', () => {
     expect(typeof fetchProducts).toBe('function');
   });
 
-  it('1.2 - É chamada a função fetch com um argumento válido', () => {
-    fetchProducts('computador');
+  it('1.2 - É chamada a função fetch com um argumento válido', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
-  it('1.3 - É retornado uma estrutura válida com um argumento válido', () => {
-    const resultado = fetchProducts('computador');
+  it('1.3 - É retornado uma estrutura válida com um argumento válido', async () => {
+    expect.assertions(1);
+    const resultado = await fetchProducts('computador');
     expect(resultado).toEqual(computadorSearch);
   });
 
-  it('1.4 - É retornado um erro se o argumento não for definido', () => {
-    expect(() => { fetchProducts(); }).toThrow(new Error('You must provide an url'));
+  it('1.4 - É retornado um erro se o argumento não for definido', async () => {
+    expect.assertions(1);
+    await expect(fetchProducts()).rejects.toEqual(new Error('You must provide an url'));
   }); 
 });
