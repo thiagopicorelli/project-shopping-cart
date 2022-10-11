ccd78f7da2bd6e042dce6afc06fcd54e686ee68a
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-10-11 17:11:42 -0300

    Cria função fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,10 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (id) => {
+  if (query === undefined) {
+    throw new Error('You must provide an url');
+  }
+
+  return fetch(`https://api.mercadolibre.com/items/${id}`)
+         .then((response) => response.json());
 };
 
 if (typeof module !== 'undefined') {
